544ffb8d12adae916ff3030a5dd4584eb8a9cc87
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-07-03 11:44:53 +0100

    start project

---
+++ src/main/java/Events/TimiEventBookingApp/TimiEventBookingAppApplication.java
@@ -0,0 +1,13 @@
+package Events.TimiEventBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimiEventBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimiEventBookingAppApplication.class, args);
+	}
+
+}
